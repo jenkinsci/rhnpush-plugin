45013757cbb945af8af4dbc38e9bc057fc0655f1
Marco Tizzoni <marco.tizzoni@gmail.com>
2014-03-18 22:18:03 +0100

    Add --force option for rhnpush

--- src/main/java/jenkins/plugins/rhnpush/RhnPush.java
+++ src/main/java/jenkins/plugins/rhnpush/RhnPush.java
@@ -1,6 +1,5 @@
 package jenkins.plugins.rhnpush;
 
-import jenkins.plugins.rhnpush.Messages;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.Launcher;
@@ -35,12 +34,13 @@ import java.util.StringTokenizer;
 public class RhnPush extends Recorder {
 
   private List<RhnPushEntry> entries = Collections.emptyList();
-  private boolean deployEvenBuildFail;
-  private boolean noGpg;
+  private final boolean deployEvenBuildFail;
+  private final boolean noGpg;
+  private final boolean force;
   private String server;
   private String username;
   private Secret password;
-  private String serverType;
+  private final String serverType;
   private String satelliteServerHostname;
 
   @DataBoundConstructor
@@ -51,10 +51,12 @@ public class RhnPush extends Recorder {
                   Secret password,
                   boolean deployEvenBuildFail,
                   boolean noGpg,
+                  boolean force,
                   List<RhnPushEntry> publishedRpms) {
     this.entries = publishedRpms;
     this.deployEvenBuildFail = deployEvenBuildFail;
     this.noGpg = noGpg;
+    this.force = force;
     if (this.entries == null) {
       this.entries = Collections.emptyList();
     }
@@ -153,7 +155,11 @@ public class RhnPush extends Recorder {
             if (noGpg) {
               command.add("--nosig");
             }
-
+            
+            if (force) {
+              command.add("--force");
+            }
+            
             for (FilePath rpmFilePath : matchedRpms) {
               command.add(rpmFilePath.toURI().normalize().getPath());
             }
